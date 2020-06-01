e5b23d641665b9632c357f2cde28f7c13d122020
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-01 22:39:22 +0100

    feat: add simple bfs

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,17 +1,54 @@
-
-def zipper():
-    fruits = ['apples', 'oranges', 'bananas']
-    quantities = [5, 3, 4]
-    prices = [1.50, 2.25, 0.89]
-
-    i = 0
-    output = []
-    for fruit in fruits:
-        for qty in quantities:
-            for price in prices:
-                output.append((fruit, qty, price))
-        i += 1
-    return output
+import collections
 
 
-print(zipper())
+class SimpleGraph:
+    def __init__(self):
+        self.edges = {}
+
+    def neighbors(self, id):
+        return self.edges[id]
+
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
+
+
+def breadth_first_search_1(graph, start):
+    # print out what we find
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    print(visited)
+
+    while not frontier.empty():
+        current = frontier.get()
+        # print("Visiting %r" % current)
+        for next in graph.neighbors(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
+                print(visited)
+
+
+example_graph = SimpleGraph()
+example_graph.edges = {
+    'A': ['B'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A'],
+    'D': ['E', 'A'],
+    'E': ['B']
+}
+
+breadth_first_search_1(example_graph, 'A')
