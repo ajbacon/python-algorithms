9155154c918108d1407378f7dfedac04cc469440
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-02 13:43:34 +0100

    feat: own bfs implementation

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,54 +1,27 @@
-import collections
-
-
-class SimpleGraph:
-    def __init__(self):
-        self.edges = {}
-
-    def neighbors(self, id):
-        return self.edges[id]
-
-
-class Queue:
-    def __init__(self):
-        self.elements = collections.deque()
-
-    def empty(self):
-        return len(self.elements) == 0
-
-    def put(self, x):
-        self.elements.append(x)
-
-    def get(self):
-        return self.elements.popleft()
+from test import *
+from collections import deque
 
 
 def breadth_first_search_1(graph, start):
-    # print out what we find
-    frontier = Queue()
-    frontier.put(start)
-    visited = {}
-    visited[start] = True
-
-    print(visited)
-
-    while not frontier.empty():
-        current = frontier.get()
-        # print("Visiting %r" % current)
-        for next in graph.neighbors(current):
-            if next not in visited:
-                frontier.put(next)
-                visited[next] = True
-                print(visited)
+    visited = [start]
+    search_queue = deque()
+    search_queue.extend(graph[start])
+    print(f'visited: {start}')
+    while search_queue:
+        current = search_queue.popleft()
+        if not current in visited:
+            print(f'visited: {current}')
+            visited.append(current)
+            search_queue.extend(graph[current])
 
 
-example_graph = SimpleGraph()
-example_graph.edges = {
-    'A': ['B'],
-    'B': ['A', 'C', 'D'],
-    'C': ['A'],
-    'D': ['E', 'A'],
-    'E': ['B']
+example_graph = {
+    'A': ['B', 'C'],
+    'B': ['E'],
+    'C': ['D'],
+    'D': ['E'],
+    'E': ['F'],
+    'F': []
 }
 
 breadth_first_search_1(example_graph, 'A')
---
+++ graphs/graphs.py
@@ -0,0 +1,11 @@
+from test import *
+
+a = from_id_width(31, 30)
+print(a)
+
+g = SquareGrid(5, 5)
+
+print(list(g.neighbors((0, 0))))
+
+
+print(DIAGRAM1_WALLS)
