4aa49c92b85bec75abaa46962cfb42a4bd91ae30
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-15 20:35:37 +0100

    test: complete testing

--- misc/sum_of_intervals.py
+++ misc/sum_of_intervals.py
@@ -33,6 +33,8 @@ def sum_of_intervals(intervals):
 # new_arr holds output at space O(n)
 # hence overall space complexity O(n)
 
+# relatively inefficient solution, this can probably be improved
+
 
 # ------------------------------TESTING-------------------------------------
 import unittest
@@ -43,35 +45,89 @@ class CoinChange(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_simple_1_denomination_sum1(self):
-        """it should return 1 given denominations of [1] and target of 1"""
-        denoms = [1]
-        target = 1
-        res = change(denoms, len(denoms), target)
-        self.assertEqual(res, 1)
+    def test_one_interval(self):
+        """it should return 3 given one interval of [(1,4)]"""
+        intervals = [
+            (1, 4)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 3)
+
+    def test_two_intervals_no_overlap(self):
+        """it should return 6 given two intervals [(1,4), (6,9)]"""
+        intervals = [
+            (1, 4),
+            (6, 9)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 6)
+
+    def test_two_intervals_with_overlap(self):
+        """it should return 8 given two intervals [(1,4), (3,9)]"""
+        intervals = [
+            (1, 4),
+            (3, 9)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 8)
+
+    def test_two_intervals_with_complete_overlap(self):
+        """it should return the correct result given complete overlap of second interval"""
+        intervals = [
+            (1, 10),
+            (3, 9)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 9)
+
+    def test_three_intervals_mixture_of_overlaps(self):
+        """it should return 7 given three unordered intervals [(1,4), (7,10), (3,5)]"""
+        intervals = [
+            (1, 4),
+            (7, 10),
+            (3, 5)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 7)
+
+    def test_many_intervals_mixture_of_overlaps(self):
+        """it should return 19 given many unordered and various overlapping """
+        intervals = [
+            (1, 5),
+            (10, 20),
+            (1, 6),
+            (16, 19),
+            (5, 11)
+        ]
+        res = sum_of_intervals(intervals)
+        self.assertEqual(res, 19)
 
 
-intervals = [
-    (1, 4)
-]
-res = sum_of_intervals(intervals)
-print(res, res == 3)
+if __name__ == '__main__':
+    unittest.main()
 
 
-intervals = [
-    (1, 4),
-    (7, 10),
-    (3, 5)
-]
-res = sum_of_intervals(intervals)
-print(res, res == 7)
+# intervals = [
+#     (1, 4)
+# ]
+# res = sum_of_intervals(intervals)
+# print(res, res == 3)
 
-intervals = [
-    (1, 5),
-    (10, 20),
-    (1, 6),
-    (16, 19),
-    (5, 11)
-]
-res = sum_of_intervals(intervals)
-print(res, res == 19)
+
+# intervals = [
+#     (1, 4),
+#     (7, 10),
+#     (3, 5)
+# ]
+# res = sum_of_intervals(intervals)
+# print(res, res == 7)
+
+# intervals = [
+#     (1, 5),
+#     (10, 20),
+#     (1, 6),
+#     (16, 19),
+#     (5, 11)
+# ]
+# res = sum_of_intervals(intervals)
+# print(res, res == 19)
